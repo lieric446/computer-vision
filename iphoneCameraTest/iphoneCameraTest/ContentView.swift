48a3884092b356c71d21a6dbba307578fa4a12e6
eli7 <lieric446@gmail.com>
2025-12-29 15:10:50 -0500

    template to try out iphone lidar

---
+++ iphoneCameraTest/iphoneCameraTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iphoneCameraTest
+//
+//  Created by Eric Li on 12/29/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iphoneCameraTest/iphoneCameraTest/iphoneCameraTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  iphoneCameraTestApp.swift
+//  iphoneCameraTest
+//
+//  Created by Eric Li on 12/29/25.
+//
+
+import SwiftUI
+
+@main
+struct iphoneCameraTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
